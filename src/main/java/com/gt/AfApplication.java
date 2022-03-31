33c07898c0c3da655c8f08c63268949110d6342d
zhukeyan <keye520520>
2022-03-31 17:52:26 +0800

    新增测试项目

--- src/main/java/com/gt/AfApplication.java
+++ src/main/java/com/gt/AfApplication.java
@@ -1,13 +1,17 @@
 package com.gt;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
+@MapperScan("com.gt.**.mapper")
 public class AfApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AfApplication.class, args);
+        System.out.println("-------------------------------------------------------启动成功------------------------------------------------------------------------");
     }
 
 }
