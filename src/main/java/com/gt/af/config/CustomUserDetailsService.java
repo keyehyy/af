3b2be6eba8abe4d7b1cd6e3ee1f13104b0475b4f
zhukeyan <keye520520>
2022-04-01 15:54:39 +0800

    新增测试项目

--- src/main/java/com/gt/af/config/CustomUserDetailsService.java
+++ src/main/java/com/gt/af/config/CustomUserDetailsService.java
@@ -1,53 +1,53 @@
-//package com.gt.af.config;
-//
-//import com.gt.af.s.model.UserInfo;
-//import com.gt.af.s.service.UserInfoService;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.authority.SimpleGrantedAuthority;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.stereotype.Component;
-//
-//import javax.annotation.Resource;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-///**
-// * @Author qt
-// * @Date 2021/3/25
-// * @Description
-// */
-//
-//@Component
-//public class CustomUserDetailsService implements UserDetailsService {
-//    @Resource
-//    private UserInfoService userInfoService;
-//    @Resource
-//    private PasswordEncoder passwordEncoder;
-//
-//    public CustomUserDetailsService(UserInfoService userInfoService) {
-//        this.userInfoService = userInfoService;
-//    }
-//
-//    @Override
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        /**
-//         * 1/通过userName 获取到userInfo信息
-//         * 2/通过User（UserDetails）返回details。
-//         */
-//        //通过userName获取用户信息
-//        UserInfo userInfo = userInfoService.getUserInfoByUsername(username);
-//        if(userInfo == null) {
-//            throw new UsernameNotFoundException("查询不到用户信息");
-//        }
-//        //定义权限列表.
-//        List<GrantedAuthority> authorities = new ArrayList<>();
-//        // 用户可以访问的资源名称（或者说用户所拥有的权限） 注意：必须"ROLE_"开头
-//        authorities.add(new SimpleGrantedAuthority("ROLE_"+ userInfo.getRid()));
-//        User userDetails = new User(userInfo.getUsername(),passwordEncoder.encode(userInfo.getPassword()),authorities);
-//        return userDetails;
-//    }
-//}
+package com.gt.af.config;
+
+import com.gt.af.s.model.UserInfo;
+import com.gt.af.s.service.UserInfoService;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author qt
+ * @Date 2021/3/25
+ * @Description
+ */
+
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+    @Resource
+    private UserInfoService userInfoService;
+    @Resource
+    private PasswordEncoder passwordEncoder;
+
+    public CustomUserDetailsService(UserInfoService userInfoService) {
+        this.userInfoService = userInfoService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        /**
+         * 1/通过userName 获取到userInfo信息
+         * 2/通过User（UserDetails）返回details。
+         */
+        //通过userName获取用户信息
+        UserInfo userInfo = userInfoService.getUserInfoByUsername(username);
+        if(userInfo == null) {
+            throw new UsernameNotFoundException("查询不到用户信息");
+        }
+        //定义权限列表.
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        // 用户可以访问的资源名称（或者说用户所拥有的权限） 注意：必须"ROLE_"开头
+        authorities.add(new SimpleGrantedAuthority("ROLE_"+ userInfo.getRid()));
+        User userDetails = new User(userInfo.getUsername(),passwordEncoder.encode(userInfo.getPassword()),authorities);
+        return userDetails;
+    }
+}
--- src/main/java/com/gt/af/security/CustomLogoutSuccessHandler.java
+++ src/main/java/com/gt/af/security/CustomLogoutSuccessHandler.java
@@ -1,30 +1,30 @@
-//package com.gt.af.security;
-//
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
-//
-//import javax.servlet.ServletException;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//
-///**
-// * @desc 退出登录处理
-// * @author zhukeyan
-// * @date
-// */
-//
-//@Slf4j
-//public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
-//
-//    @Override
-//    public void onLogoutSuccess(HttpServletRequest httpServletRequest, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-//        System.out.println("退出成功!");
-//        //这里写你登录成功后的逻辑
-//        response.setStatus(HttpStatus.OK.value());
-//        response.setContentType("application/json;charset=UTF-8");
-//        response.getWriter().write("退出成功!");
-//    }
-//}
+package com.gt.af.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @desc 退出登录处理
+ * @author zhukeyan
+ * @date
+ */
+
+@Slf4j
+public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
+
+    @Override
+    public void onLogoutSuccess(HttpServletRequest httpServletRequest, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        System.out.println("退出成功!");
+        //这里写你登录成功后的逻辑
+        response.setStatus(HttpStatus.OK.value());
+        response.setContentType("application/json;charset=UTF-8");
+        response.getWriter().write("退出成功!");
+    }
+}
